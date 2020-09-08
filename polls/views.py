3e5ee1dc2d3163a5d379cff3c0a717e5b84d981e
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2020-09-08 16:28:23 +0200

    created vote view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 
 
@@ -30,3 +30,20 @@ def results(request, question_id):
         'question': question,
     }
     return render(request, 'polls/results.html', context)
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+
+    except (KeyError, Choice.DoesNotExist):
+        context = {
+            'question': question, 
+            'error_message': 'please select any choice'}
+        return render(request, 'polls/detail.html', context)
+    
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
