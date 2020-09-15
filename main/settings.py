5c48faf0680071c96467bcc04486723e4f7772af
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2020-09-15 15:11:08 +0200

    added login and logout urls variables to the main settings

--- main/settings.py
+++ main/settings.py
@@ -138,3 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'index' 
+LOGOUT_REDIRECT_URL = 'users:login'
