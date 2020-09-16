14faad41319ff2334d83765856ac6fe0fdaba20b
mm.kucharski@gmail.com <mm.kucharski@gmail.com>
2020-09-16 11:46:45 +0200

    create message when user is created

--- users/views.py
+++ users/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.messages.views import SuccessMessageMixin
 
 
-class UserCreateView(CreateView):
+class UserCreateView(SuccessMessageMixin, CreateView):
     form_class = UserCreationForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+    success_message = 'Account created, you can now login !'
